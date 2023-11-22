a74a9f36fdf1a53146dbf6c94c7ca6cc48c15496
normanardian <normanardian24@gmail.com>
2023-11-22 09:13:10 +0700

    Update Sendwa.php

--- application/models/Sendwa.php
+++ application/models/Sendwa.php
@@ -4,14 +4,14 @@ class Sendwa extends CI_Model
 {
     function kirim2($phone, $msg)
     {
-        $link  =  "https://kudus.wablas.com";
+        $link  =  "https://pati.wablas.com";
         $data = [
             'phone' => $phone,
             'message' => $msg,
         ];
 
         $curl = curl_init();
-        $token =  "jl2EXREahnoSNBxq125y59aZgRwQy8Cf9skGbEermoRy7nxWyKvD7hg4X6WPrkAE";
+        $token =  "QQyBPckMO6InL8uvhvuqXa44THR0FjZM1EgBu4aKJ6bRRR8HQ7JHKljx8ZpibCqr";
         curl_setopt(
             $curl,
             CURLOPT_HTTPHEADER,
@@ -32,7 +32,7 @@ class Sendwa extends CI_Model
     public function kirim($phone, $msg)
     {
         $curl = curl_init();
-        $token = "jl2EXREahnoSNBxq125y59aZgRwQy8Cf9skGbEermoRy7nxWyKvD7hg4X6WPrkAE";
+        $token = "QQyBPckMO6InL8uvhvuqXa44THR0FjZM1EgBu4aKJ6bRRR8HQ7JHKljx8ZpibCqr";
         $data = [
             'phone' => $phone,
             'message' => $msg,
@@ -47,7 +47,7 @@ class Sendwa extends CI_Model
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_URL, "https://kudus.wablas.com");
+        curl_setopt($curl, CURLOPT_URL, "https://pati.wablas.com");
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
         $result = curl_exec($curl);
@@ -56,7 +56,7 @@ class Sendwa extends CI_Model
     }
     public function pickup($phone, $msg)
     {
-        $result = file_get_contents("https://kudus.wablas.com/api/send-message?token=jl2EXREahnoSNBxq125y59aZgRwQy8Cf9skGbEermoRy7nxWyKvD7hg4X6WPrkAE&phone=$phone&message=$msg");
+        $result = file_get_contents("https://pati.wablas.com/api/send-message?token=QQyBPckMO6InL8uvhvuqXa44THR0FjZM1EgBu4aKJ6bRRR8HQ7JHKljx8ZpibCqr&phone=$phone&message=$msg");
         return $result;
         // echo "<pre>";
         // print_r($result);
