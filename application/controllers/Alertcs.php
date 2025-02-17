9601d7f926df71b55cba4b06cb5932da705f2f32
normanardian <normanardian24@gmail.com>
2025-02-17 17:12:45 +0700

    update city

---
+++ application/controllers/Alertcs.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Alertcs extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Sendwa', 'wa');
+        $this->db->query("SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));");
+    }
+    public function alertSubmitSo()
+    {
+
+
+        $user = $this->db->query("SELECT id_user,no_hp,id_role,nama_user FROM tb_user WHERE id_role = 4");
+        foreach ($user->result_array() as $user1) {
+            $so = $this->db->query("SELECT id_so,shipper,tgl_pickup,status FROM tbl_so WHERE tgl_pickup = '" . date('Y-m-d') . "' AND cancel_date IS NULL AND submitso_at IS NULL AND id_sales = " . $user1['id_user'] . " AND status > 0 AND status != 5");
+
+            if ($so->num_rows() != NULL) {
+                $listSo = '';
+                foreach ($so->result_array() as $so1) {
+                    $cekResi = $this->db->query("SELECT shipment_id FROM tbl_shp_order WHERE id_so = " . $so1['id_so'] . " LIMIT 1 ");
+                    if ($cekResi->num_rows() != 0) {
+                        $listSo .= '\r\n' . $so1['shipper'] . ' Tanggal Pickup: ' . $so1['tgl_pickup'];
+                    }
+                }
+                $pesan = "Halo " . $user1['nama_user'] . ", Ada SO yang belum di submit harganya, berikut SO yang terlampir $listSo";
+                // var_dump($listSo);
+                $this->wa->pickup('+' . $user1['no_hp'], "$pesan");
+                $this->wa->pickup('+6285697780467', "$pesan");
+            }
+        }
+    }
+
+    public function alertSubmitSoIncoming()
+    {
+
+
+        $user = $this->db->query("SELECT id_user,no_hp,id_role,nama_user FROM tb_user WHERE id_role = 4");
+        foreach ($user->result_array() as $user1) {
+            $so = $this->db->query("SELECT id_so,shipper,tgl_pickup,status FROM tbl_so WHERE DATE(created_at) = '" . date('Y-m-d') . "' AND cancel_date IS NULL AND is_incoming = 1 AND submitso_at IS NULL AND id_sales = " . $user1['id_user'] . " AND status > 0 AND status != 5");
+
+            if ($so->num_rows() != NULL) {
+                $listSo = '';
+                foreach ($so->result_array() as $so1) {
+                    $cekResi = $this->db->query("SELECT shipment_id FROM tbl_shp_order WHERE id_so = " . $so1['id_so'] . " LIMIT 1 ");
+                    if ($cekResi->num_rows() != 0) {
+                        $listSo .= '\r\n' . $so1['shipper'] . ' Tanggal Pickup: ' . $so1['tgl_pickup'];
+                    }
+                }
+                $pesan = "Halo " . $user1['nama_user'] . ", Ada SO Incoming yang belum di submit harganya, berikut SO yang terlampir $listSo";
+                // var_dump($listSo);
+                $this->wa->pickup('+' . $user1['no_hp'], "$pesan");
+                $this->wa->pickup('+6285697780467', "$pesan");
+            }
+        }
+    }
+
+    // alertLeadTime 
+    public function alertLeadTime()
+    {
+        $resi = $this->db->query("SELECT a.shipment_id,a.shipper,a.city_consigne,a.tgl_pickup,b.lead_min,b.lead_max  FROM tbl_shp_order a JOIN tb_city b on a.city_consigne = b.city_name WHERE a.deleted = 0 AND a.tgl_diterima IS NULL AND b.lead_min != 0 AND b.lead_max != 0 AND YEAR(a.tgl_pickup) >= 2025");
+        $listResi = '';
+        foreach ($resi->result_array() as $resi1) {
+            $tgl_pickup = date('Y-m-d', strtotime($resi1['tgl_pickup']));
+            $tgl_sekarang = date('Y-m-d');
+
+            // JARAK DARI TANGGAL SEKARANG KE TANGGAL PICKUP 
+            $jarak = (strtotime($tgl_sekarang) - strtotime($tgl_pickup)) / (60 * 60 * 24);
+            if ($jarak >= $resi1['lead_max']) {
+                $listResi .= '\r\n\r\n' . $resi1['shipment_id'] . ' Customer ' . $resi1['shipper'] . ' Tujuan ' . $resi1['city_consigne'] . ' Pickup tanggal ' . date('d-m-Y',strtotime($resi1['tgl_pickup'])) .' Max Pengiriman : ' . $resi1['lead_max'].' Hari, Waktu yang telah dilewati : ' . $jarak . ' Hari';
+            }
+            
+        }
+        $pesan = "Halo CS, Ada Resi yang sudah melewati lead time, berikut resi yang terlampir $listResi <br><br> Silahkan di cek kembali dan Update tanggal diterimanya. Terima Kasih";
+        $this->wa->pickup('+6285697780467', "$pesan");
+
+
+
+    }
+}
--- application/controllers/superadmin/City.php
+++ application/controllers/superadmin/City.php
@@ -1,6 +1,12 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Reader\Csv;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx as ReaderXlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
 class City extends CI_Controller
 {
 	public function __construct()
@@ -61,6 +67,121 @@ class City extends CI_Controller
 			redirect('superadmin/city');
 		} else {
 			$this->session->set_flashdata('message', 'Diedit');
+			redirect('superadmin/city');
+		}
+	}
+
+	public function editTreeCodeBulky()
+	{
+		// from file excel 
+		$this->db->trans_start();
+		try {
+			$file = $_FILES['file']['name'];
+			$ext = pathinfo($file, PATHINFO_EXTENSION);
+			if ($ext == 'csv') {
+				$reader = new Csv();
+			} else {
+				$reader = new ReaderXlsx();
+			}
+			$reader->setReadDataOnly(true);
+			$spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+			$sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+
+
+			foreach ($sheetData as $key => $value) {
+				if ($key > 0) {
+					$id_city = $value[0];
+					$tree_code = $value[2];
+					// remove space tree code 
+					$tree_code = str_replace(' ', '', $tree_code);
+					if ($tree_code != '' || $tree_code != null) {
+						$update = $this->db->update('tb_city', ['tree_code' => $tree_code], ['id_city' => $id_city]);
+						if ($update) {
+							$response = array('status' => 'success', 'message' => 'Berhasil diupdate');
+						} else {
+							throw new Exception('City' . $value[1] . ' gagal diupdate');
+						}
+					}
+				}
+			}
+			$this->db->trans_complete();
+			if ($this->db->trans_status() === FALSE) {
+				throw new Exception('Transaction failed');
+			} else {
+				$response = array('status' => 'success', 'message' => 'Berhasil diupdate');
+			}
+		} catch (Exception $e) {
+			$this->db->trans_rollback();
+			$response = array('status' => 'error', 'message' => $e->getMessage());
+		}
+
+		if ($response['status'] == 'success') {
+			$this->session->set_flashdata('message', 'Berhasil diupdate');
+			redirect('superadmin/city');
+		} else {
+			$this->session->set_flashdata('message', $response['message']);
+			redirect('superadmin/city');
+		}
+	}
+
+	public function editLeadBulky()
+	{
+		// from file excel 
+		$this->db->trans_start();
+		try {
+			$file = $_FILES['file']['name'];
+			$ext = pathinfo($file, PATHINFO_EXTENSION);
+			if ($ext == 'csv') {
+				$reader = new Csv();
+			} else {
+				$reader = new ReaderXlsx();
+			}
+			$reader->setReadDataOnly(true);
+			$spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+			$sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+
+
+			foreach ($sheetData as $key => $value) {
+				if ($key > 0) {
+					$id_city = $value[0];
+					$lead_min = $value[3];
+					$lead_max = $value[4];
+					// remove space tree code 
+					$lead_min = str_replace(' ', '', $lead_min);
+					$lead_max = str_replace(' ', '', $lead_max);
+					if ($lead_min != '' || $lead_min != null) {
+						// if lead min max number 
+						if (!is_numeric($lead_min) || !is_numeric($lead_max)) {
+							throw new Exception('Lead min max harus angka');
+						} else {
+							$update = $this->db->update('tb_city', ['lead_min' => $lead_min, 'lead_max' => $lead_max], ['id_city' => $id_city]);
+							if ($update) {
+								$response = array('status' => 'success', 'message' => 'Berhasil diupdate');
+							} else {
+								throw new Exception('City' . $value[1] . ' gagal diupdate leadnya');
+							}
+						}
+					}
+				}
+			}
+			$this->db->trans_complete();
+			if ($this->db->trans_status() === FALSE) {
+				throw new Exception('Transaction failed');
+			} else {
+				$response = array('status' => 'success', 'message' => 'Berhasil diupdate');
+			}
+		} catch (Exception $e) {
+			$this->db->trans_rollback();
+			$response = array('status' => 'error', 'message' => $e->getMessage());
+		}
+
+		if ($response['status'] == 'success') {
+			$this->session->set_flashdata('message', 'Berhasil diupdate');
+			redirect('superadmin/city');
+		} else {
+			$this->session->set_flashdata('message', $response['message']);
 			redirect('superadmin/city');
 		}
 	}
